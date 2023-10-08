19f5b4773301c92c6eda07e261fcbe554d668b23
Alexander Gomez <mpalexanderg@gmail.com>
2023-10-08 14:22:04 -0500

    Se corrigio error de conexión

--- SWBiblioteca/Program.cs
+++ SWBiblioteca/Program.cs
@@ -5,10 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConectionDB")));
 
 var app = builder.Build();
 
-builder.Services.AddDbContext<ApplicationDbContext>(z => z.UseSqlServer("name = ConectionDB"));
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
