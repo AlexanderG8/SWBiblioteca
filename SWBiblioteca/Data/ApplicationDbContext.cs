5b7c7dd87a2e23e37ecef9c43f73faf53b963bc2
Alexander Gomez <mpalexanderg@gmail.com>
2023-10-08 14:17:40 -0500

    MGG: Se desarrollo arquitectura de proyecto SWBiblioteca

---
+++ SWBiblioteca/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SWBiblioteca.Entity;
+using System.Reflection.Emit;
+
+namespace SWBiblioteca.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options) 
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder model)
+        {
+            base.OnModelCreating(model);
+            //builder.Entity<entidad>().ToTable("nombretabla", "esquema");
+            model.Entity<TIPO_PERSONA>().HasIndex(z => z.IdTipoPersona).IsUnique();
+        }
+
+        public DbSet<TIPO_PERSONA> TIPO_PERSONA { get; set; }
+
+    }
+}
